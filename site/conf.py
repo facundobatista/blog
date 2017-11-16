0e1f8dd380810853801e0494c57ad000db336ab0
marian-vignau <mavignau@gmail.com>
2017-11-16 16:41:38 -0300

    Changed SITE_URL to test DuckDuckGo search

--- site/conf.py
+++ site/conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "Facundo Batista"  # (translatable)
 BLOG_TITLE = "Bitácora de vuelo"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "http://www.taniquetil.com.ar/blog/"
+SITE_URL = "http://beta.taniquetil.com.ar/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # 0BASE_URL = "http://www.taniquetil.com.ar/plog/"
