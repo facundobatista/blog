6f8c16ae78b84eb51f6b314960a638bb5a02bbdc
marian-vignau <mavignau@gmail.com>
2018-02-08 17:20:46 -0300

    Put up to five posts in index pages

--- site/conf.py
+++ site/conf.py
@@ -1136,7 +1136,7 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 
 # Modify the number of Post per Index Page
 # Defaults to 10
-# INDEX_DISPLAY_POST_COUNT = 10
+INDEX_DISPLAY_POST_COUNT = 5
 
 # By default, Nikola generates RSS files for the website and for tags, and
 # links to it.  Set this to False to disable everything RSS-related.
