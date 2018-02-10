b65bc13f21c1222b2a394e37d328f24a034cc7ba
marian-vignau <mavignau@gmail.com>
2018-02-09 22:56:33 -0300

    Fix URL to be coincident with printer_friendly style status

--- site/themes/blogtxt/assets/js/printer-friendly.js
+++ site/themes/blogtxt/assets/js/printer-friendly.js
@@ -13,26 +13,27 @@ function get_printer_friendly(str) {
 $( document ).ready(switch_printer_friendly ( get_printer_friendly(locate)));
 
 function switch_printer_friendly ( show_printer_friendly ) {
-   var i, a;
+    var i, a, x;
 
-   for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-     if(a.getAttribute("rel").indexOf("style") != -1
+    //Enable or disable printer_friendly css style sheet
+    for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+        if(a.getAttribute("rel").indexOf("style") != -1
         && a.getAttribute("title")) {
-       if(a.getAttribute("title") == "printer-friendly") {
-           a.disabled = !show_printer_friendly;
+            if(a.getAttribute("title") == "printer-friendly")
+               a.disabled = !show_printer_friendly;
+        }
+    }
 
-           // Change link to enable/disable printer friendly
-           x = document.getElementById("printer_friendly_link");
-           if (show_printer_friendly) {
-              x.setAttribute('onclick','switch_printer_friendly( false ); return false;');
-              x.setAttribute("title", "Vista Normal");
-              x.innerHTML = `<img class="theme-icon" src="/assets/images/normal-view-icon.png" alt="Vista Normal" />`;
-           } else {
-              x.setAttribute('onclick','switch_printer_friendly( true ); return false;');
-              x.innerHTML = `<img class="theme-icon" src="/assets/images/print-icon.png" alt="Imprimir" />`;
-           }
-
-       }
-     }
+   // Change link to enable/disable printer friendly
+   x = document.getElementById("printer_friendly_link");
+   if (show_printer_friendly) {
+      x.setAttribute('onclick','window.location = window.location.pathname;');
+      x.setAttribute("title", "Vista Normal");
+      x.innerHTML = `<img class="theme-icon" src="/assets/images/normal-view-icon.png" alt="Vista Normal" />`;
+   } else {
+      x.setAttribute('onclick','window.location = window.location.pathname + "?style=printer_friendly";');
+      x.setAttribute("title", "Imprimir");
+      x.innerHTML = `<img class="theme-icon" src="/assets/images/print-icon.png" alt="Imprimir" />`;
    }
+
 }
