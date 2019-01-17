68e8a43b1fe9fe3f460e5a563adc2f305dd7395d
Jose Luis <joseluiszanotti@gmail.com>
2019-01-17 13:51:06 -0300

    all fixed after 3 days, damn

--- site/themes/blogtxt/assets/js/alt-image-hack.js
+++ site/themes/blogtxt/assets/js/alt-image-hack.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     $.each($(".entry-content img"), function() {
+        var imgSrc = $(this)
         var altText = $(this).attr('alt');
+
         $(this).wrap('<figure>');
         $(this).after('<figcaption>'+altText+'</figcaption>');
+
     });
 });
