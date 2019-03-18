b9a942fb65761797c0de66de55205a77fb2f2ae8
Facundo Batista <facundo@taniquetil.com.ar>
2019-03-18 11:07:28 -0300

    More posts with fixed urls.

--- utilities/checkurls.py
+++ utilities/checkurls.py
@@ -7,7 +7,9 @@ import sys
 from urllib import request
 
 headers = {
-    'User-Agent': "Mozilla/5.0",
+    'User-Agent': (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"),
 }
 
 socket.setdefaulttimeout(5)
